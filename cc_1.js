bd0a1171aa43019061ed26a22cd847cf2ebf415b
Tristen Roupp <tristenr@usf.edu>
2024-05-22 13:46:50 -0600

    Add files via upload

--- cc_1.js
+++ cc_1.js
@@ -1,31 +1,61 @@
-var tipPercent;
-var billAmount = 100;
+var tip;
+var billAmount = 275;
 var totalCost;
 
-const bills = [275, 40, 430, 1000];
-const tips = [];
-const totalCosts = [];
+tip = (billAmount > 50 && billAmount < 300) ? 0.15 : 0.2;
+console.log("Tip percent is: ", tip);
 
+//This function takes the billAmount as the parameter and then calculates the tip amount 
 function calcTip(billAmount){
     tipPercent = (billAmount > 50 && billAmount < 300) ? 0.15 : 0.2;
     var tipAmount = billAmount * tipPercent; // Corrected this line
     return tipAmount;
 }
-const realTipAmount = calcTip(billAmount);
-totalCost = billAmount + realTipAmount;
-console.log("The bill is: $", billAmount);
-console.log("The tip is: $" + realTipAmount.toFixed(2));
-console.log("The total cost is: $", totalCost);
 
-bills.forEach(function(bill) {
-    const tipAmount = calcTip(bill);
+// Test the calcTip function with a bill value of 275
+const testTip = calcTip(billAmount);
+console.log("Tip amount is: $", testTip);
+
+// Calculate total cost
+totalCost = testTip + billAmount;
+
+// Output the bill amount, tip, and total cost
+console.log("The bill amount was, ", billAmount, "the tip was, $", testTip, "The total cost was, $",totalCost);
+
+//Test data arrays
+const billsSet1 = [275, 40, 430];
+const billsSet2 = [125, 555, 44];
+
+const tips= [];
+
+// Populate tips array with the results of calling calcTip for each bill value in billsSet1
+billsSet1.forEach(function(i){
+    const tipAmount = calcTip(i);
     tips.push(tipAmount);
-    totalCosts.push(billAmount + tipAmount);
 });
-console.log();
-bills.forEach(function(bill, index) {
-    console.log("The bill is: $" + bill);
-    console.log("The tip is: $" + tips[index].toFixed(2));
-    console.log("The total cost is: $" + totalCosts[index].toFixed(2));
-    console.log();
+
+// Populate tips array with the results of calling calcTip for each bill value in billsSet1
+console.log("Data set 2");
+billsSet2.forEach(function(i){
+    const tipAmount = calcTip(i);
+    tips.push(tipAmount);
 });
+
+// Populate totals array with the sum of each bill and its respective tip in billsSet1
+const totalsBillSet1 = [];
+billsSet1.forEach(function(billAmount){
+    const tipAmount = calcTip(billAmount);
+    totalsBillSet1.push(billAmount + tipAmount);
+});
+
+// Populate totals array with the sum of each bill and its respective tip in billsSet2
+const totalsBillSet2 = [];
+billsSet2.forEach(function(billAmount){
+    const tipAmount = calcTip(billAmount);
+    totalsBillSet2.push(billAmount + tipAmount);
+});
+
+//Log the results to the console 
+console.log(totalsBillSet1);
+console.log(totalsBillSet1);
+console.log(tips);
